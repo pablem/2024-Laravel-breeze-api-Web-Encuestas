442032c917e3b2bec5979e97dbe189c1a2e57929
pablem <palmierijpablo@gmail.com>
2024-06-11 20:32:04 -0300

    crud encuestados + validaciones

--- app/Http/Controllers/EncuestadoController.php
+++ app/Http/Controllers/EncuestadoController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Encuestado;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class EncuestadoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function getEncuestadosConCorreo()
     {
-        //
+        $encuestados = Encuestado::whereNotNull('correo')->get();
+        return response()->json($encuestados, 200);
     }
 
     /**
@@ -25,9 +29,43 @@ class EncuestadoController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function storeAnonimo(Request $request)
+    public function store(Request $request)
     {
-        
+        try {
+            DB::beginTransaction();
+
+            $encuestadosData = $request->json()->all();
+            $success = [];
+            $errors = [];
+
+            foreach ($encuestadosData as $data) {
+                $validator = Validator::make($data, [
+                    'correo' => 'required|email|unique:encuestados,correo',
+                ]);
+
+                if ($validator->fails()) {
+                    $errors[] = [
+                        'correo' => $data['correo'],
+                        'errors' => $validator->errors()
+                    ];
+                    continue;
+                }
+
+                $encuestado = new Encuestado([
+                    'correo' => $data['correo'],
+                    // 'ip_identificador' => null, //=> $request->ip(),
+                ]);
+                $encuestado->save();
+                $success[] = $encuestado;
+            }
+
+            DB::commit();
+            return response()->json(['success' => $success, 'errors' => $errors], 201);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -35,7 +73,7 @@ class EncuestadoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        //sin uso
     }
 
     /**
@@ -43,7 +81,7 @@ class EncuestadoController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        //sin uso
     }
 
     /**
@@ -51,14 +89,41 @@ class EncuestadoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
-    }
+        try {
+            $encuestado = Encuestado::findOrFail($id);
 
+            $validator = Validator::make($request->all(), [
+                'correo' => 'required|email|unique:encuestados,correo,' . $encuestado->id,
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $encuestado->correo = $request->correo;
+            $encuestado->save();
+
+            return response()->json(['message' => 'Encuestado actualizado correctamente'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified resource from storage. (varios ids)
      */
-    public function destroy(string $id)
+    public function destroy(Request $request)
     {
-        //
+        try {
+            DB::beginTransaction();
+
+            $ids = $request->ids;
+            $deleted = Encuestado::whereIn('id', $ids)->delete();
+
+            DB::commit();
+            return response()->json(['message' => 'Encuestados eliminados correctamente', 'deleted' => $deleted], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
